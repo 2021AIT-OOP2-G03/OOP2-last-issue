6664e18dfe862f6ca6a758a3ee929991a95c15c1
memiyuyu125gs <gjibxdfug0110@gmail.com>
2021-12-28 21:28:11 +0900

    add create & down_note

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.properties import NumericProperty
+from kivy.clock import Clock
 
 import sqlite3
 
@@ -99,7 +100,24 @@ class HomeScreen(Screen):
         self.third_score = str(score[2][0])
         
 class PlayScreen(Screen):
-   pass
+
+    dt = 1/30
+    move_y = NumericProperty(1000)
+    count = 0
+    text = StringProperty('Start')
+    dx = 5
+   
+    def update(self, *args):
+       self.move_y -= self.dx
+       self.count += 1
+       self.text = '{}'.format(self.count)
+
+
+
+    def start_game(self):
+        Clock.schedule_interval(self.update,self.dt)
+        
+
 
 sm = ScreenManager()
 sm.add_widget(HomeScreen(name='home'))
